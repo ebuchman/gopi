cb82f56ab180787459d1c97b9c138cf73c78ed5c
Ethan Buchman <ethan@coinculture.info>
2015-10-03 14:40:19 -0400

    multiplication, biotch

--- arithmetic.go
+++ arithmetic.go
@@ -5,9 +5,10 @@ type Chan chan Chan
 var C Chan
 
 type Integer struct {
-	n int  // cheating
 	X Chan //successor channel
 	Z Chan // zero channel
+
+	n int // cheating (debuging)
 }
 
 func NewInteger() *Integer {
@@ -49,7 +50,7 @@ func N(n int, i *Integer) {
 }
 
 // Copy takes an integer i and copies it out to j
-// DEF: Copy(i, j) = i.X?(a)Succ(i, j) + i.Z?(a)j.Z!(a)
+// DEF: Copy(i, j) = iX?(a)Succ(i, j) + iZ?(a).jZ!(a)
 func Copy(i, j *Integer) {
 	select {
 	case <-i.PullX():
@@ -60,14 +61,14 @@ func Copy(i, j *Integer) {
 }
 
 // Succ takes an integer i and returns the successor j = i+1
-// DEF: Succ(i, j) = j.X!(a)Copy(i, j)
+// DEF: Succ(i, j) = jX!(a).Copy(i, j)
 func Succ(i, j *Integer) {
 	j.FireX(C)
 	Copy(i, j)
 }
 
 // Add takes two integers and adds them, returning the result on w
-// DEF: Add(i, j, w) = i.X?(a)w.X!(a)Add(i, j, w) + i.Z?(a)Copy(j, w)
+// DEF: Add(i, j, w) = iX?(a).wX!(a).Add(i, j, w) + iZ?(a).Copy(j, w)
 func Add(i, j, w *Integer) {
 	select {
 	case <-i.PullX():
@@ -78,33 +79,84 @@ func Add(i, j, w *Integer) {
 	}
 }
 
+// Subtract j from i and return onto w. i must be larger than j.
+// DEF: Subtract(i, j, w) = Sub(i, j).Copy(i, w)
+func Subtract(i, j, w *Integer) {
+	Sub(i, j)
+	Copy(i, w)
+}
+
+// Takes the absolute value of the difference between i and j and copies onto w
+// DEF: Diff(i, j, w) = (new d)( sub(i, j, d) | sub(j, i, d) | d?(a).Succ(a, w)
+func Diff(i, j, w *Integer) {
+	d := make(chan *Integer)
+	go sub(i, j, d)
+	go sub(j, i, d)
+	a := <-d
+	Succ(a, w)
+}
+
+// Subtract j from i. i must be larger than j. broadcast i back on d
+// NOTE d is a channel that takes an int, so its really a channel on which we pass two channels ...
+// DEF: sub(i, j, d) = jX?(a).sub(i, j, d) + jZ?(a).d!(i)
+func sub(i, j *Integer, d chan *Integer) {
+	select {
+	case <-j.PullX():
+		<-i.PullX()
+		sub(i, j, d)
+	case <-j.PullZ():
+		d <- i
+	}
+}
+
+// Subtract j from i. i must be larger than j
+// DEF: Sub(i, j) = jX?(a).Sub(i, j) + jZ?(a)
+func Sub(i, j *Integer) {
+	select {
+	case <-j.PullX():
+		<-i.PullX()
+		Sub(i, j)
+	case <-j.PullZ():
+	}
+}
+
 // Double should copy i onto v and w
 // DEF: Double(i, v, w) = (new i1, i2)[ Dup(i, i1, i2) | Copy(i1, v) | Copy(i2, w) ]
 func Double(i, v, w *Integer) {
 	i1, i2 := NewInteger(), NewInteger()
 	go Dup(i, i1, i2)
-	go Copy(i1, v)
-	Copy(i2, w)
+	go func() {
+		Copy(i1, v)
+	}()
+	go func() {
+		Copy(i2, w)
+	}()
 }
 
 // Dup forwards i onto v and w as a precursor to being copied in Double.
-// DEF: Dup(i, v, w) = i.X?(a)[ v.X!(a) | w.X!(a) | i.Z?(a)( v.Z!(a) | w.Z!(a) ) | Dup(i, v, w) ]
+// DEF: Dup(i, v, w) = iX?(a)[ vX!(a).d!(c)| wX!(a).d!(c) | d?(a).d?(a).Dup(i, v, w) ] + iZ?(a)( vZ!(a) | wZ!(a) )
 func Dup(i, v, w *Integer) {
-	c := <-i.PullX()
-	go func() {
-		go v.FireX(c)
-		w.FireX(c)
-	}()
-	go func() {
-		z := <-i.PullZ()
+	d := make(Chan)
+	select {
+	case c := <-i.PullX():
+		go func() {
+			v.FireX(c)
+			d <- C
+		}()
+		go func() {
+			w.FireX(c)
+			d <- C
+		}()
+		_, _ = <-d, <-d
+		Dup(i, v, w)
+	case z := <-i.PullZ():
 		go v.FireZ(z)
 		w.FireZ(z)
-	}()
-	Dup(i, v, w)
+	}
 }
 
 // AddTo adds the value i to w without firing on Z
-// DEF: AddTo(i, w) = i.X?(a)w.X!(a)AddTo(i, w) + i.Z?(a)
+// DEF: AddTo(i, w) = iX?(a).wX!(a).AddTo(i, w) + iZ?(a)
 func AddTo(i, w *Integer) {
 	select {
 	case <-i.PullX():
@@ -115,15 +167,26 @@ func AddTo(i, w *Integer) {
 }
 
 // Multiply computes the product of i and j and returns it on w
-// DEF: Multiply(i, j, w) = j.X?(a)(new i1, i2)[ Double(i, i1, i2) | AddTo(i1, w) | Multiply(i2, j, w) ] + i.Z?(a)w.Z!(a)
+// DEF: Multiply(i, j, w) = jX?(a).(new i1, i2)[ Double(i, i1, i2) | AddTo(i2, w) | Multiply(i1, j, w) ] + iZ?(a).FlushFire(i, w)
 func Multiply(i, j, w *Integer) {
 	select {
 	case <-j.PullX():
 		i1, i2 := NewInteger(), NewInteger()
 		go Double(i, i1, i2)
-		go AddTo(i1, w)
-		Multiply(i2, j, w)
+		go AddTo(i2, w)
+		Multiply(i1, j, w)
 	case <-j.PullZ():
+		FlushFire(i, w)
+	}
+}
+
+// Flush out an integer and fire the zero on another
+// FlushFire(i, w) = x?(a).FlushFire(i, w) + z?(a).w!(a)
+func FlushFire(i, w *Integer) {
+	select {
+	case <-i.X:
+		FlushFire(i, w)
+	case <-i.Z:
 		w.FireZ(C)
 	}
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	checkArgs(args, 1, "must enter a command (n, copy, succ, add, double, multiply)")
+	checkArgs(args, 1, "must enter a command (n, copy, succ, add, sub, diff, double, doubler, multiply)")
 	cmd := args[0]
 	args = args[1:]
 
@@ -45,6 +45,24 @@ func main() {
 		go N(n2, i2)
 		go Add(i1, i2, res)
 		fmt.Println(result(res))
+	case "sub":
+		checkArgs(args, 2, "must enter two numbers to subtract")
+		n1 := parseInt(args[0])
+		n2 := parseInt(args[1])
+		i1, i2 := NewInteger(), NewInteger()
+		go N(n1, i1)
+		go N(n2, i2)
+		go Subtract(i1, i2, res)
+		fmt.Println(result(res))
+	case "diff":
+		checkArgs(args, 2, "must enter two numbers to diff")
+		n1 := parseInt(args[0])
+		n2 := parseInt(args[1])
+		i1, i2 := NewInteger(), NewInteger()
+		go N(n1, i1)
+		go N(n2, i2)
+		go Diff(i1, i2, res)
+		fmt.Println(result(res))
 	case "double":
 		checkArgs(args, 1, "must enter a number")
 		n := parseInt(args[0])
@@ -56,6 +74,63 @@ func main() {
 		wg.Add(2)
 		go printResult(res, wg)
 		go printResult(res2, wg)
+		wg.Wait()
+	case "doubler":
+		// doubling onto the end
+		// so the final results are the last d entries
+		checkArgs(args, 2, "must enter a number to double and the number of times to double it")
+		n := parseInt(args[0])
+		d := parseInt(args[1])
+		if d < 2 {
+			fmt.Println("number of times to double must be at least 2")
+			Exit()
+		}
+		ress := []*Integer{}
+		for i := 0; i < (d-1)*2; i++ {
+			ress = append(ress, NewInteger())
+		}
+		i := NewInteger()
+		go N(n, i)
+		latest := i
+		for i := 0; i < d-1; i++ {
+			go Double(latest, ress[2*i], ress[2*i+1])
+			latest = ress[i]
+		}
+		wg := new(sync.WaitGroup)
+		wg.Add(d)
+		for i := 0; i < d; i++ {
+			go printResult(ress[d-2+i], wg)
+		}
+		wg.Wait()
+	case "doubler2":
+		// doubling every other
+		// so the final results are every other element and the last 2
+		// (ie. (1, 2), (2, 3, 4), (2, 4, 5, 6), (2, 4, 6, 7, 8) ...
+		checkArgs(args, 2, "must enter a number to double and the number of times to double it")
+		n := parseInt(args[0])
+		d := parseInt(args[1])
+		if d < 2 {
+			fmt.Println("number of times to double must be at least 2")
+			Exit()
+		}
+		ress := []*Integer{}
+		for i := 0; i < (d-1)*2+1; i++ {
+			ress = append(ress, NewInteger())
+		}
+		i := NewInteger()
+		go N(n, i)
+		ress[0] = i
+		latest := i
+		for i := 0; i < d-1; i++ {
+			go Double(latest, ress[2*i+1], ress[2*(i+1)])
+			latest = ress[2*i+1]
+		}
+		wg := new(sync.WaitGroup)
+		wg.Add(d)
+		for i := 0; i < d-1; i++ {
+			go printResult(ress[2*(i+1)], wg)
+		}
+		go printResult(ress[2*(d-1)-1], wg)
 		wg.Wait()
 	case "multiply":
 		checkArgs(args, 2, "must enter two numbers to multiply")
